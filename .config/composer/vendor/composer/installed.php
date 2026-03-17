d1a66b4f0055984430348db3f40c4507db9a1369
Vedant Patil <vedant.waman.patil@gmail.com>
2026-03-17 18:46:30 -0400

    type shi

--- .config/composer/vendor/composer/installed.php
+++ .config/composer/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '31dcda22a802eb00f7fbde30521a5cb2999ae00a',
+        'reference' => '4c9e8094852dc3b937a93b50f62bf73fa6611d2c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '31dcda22a802eb00f7fbde30521a5cb2999ae00a',
+            'reference' => '4c9e8094852dc3b937a93b50f62bf73fa6611d2c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
--- .config/sketchybar/items/spaces.sh
+++ .config/sketchybar/items/spaces.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15")
+SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9")
 
-# Destroy space on right click, focus space on left click.
-# New space by left clicking separator (>)
-
-sid=0
-spaces=()
 for i in "${!SPACE_ICONS[@]}"
 do
   sid=$(($i+1))
 
   space=(
-    associated_space=$sid
     icon=${SPACE_ICONS[i]}
     icon.padding_left=10
     icon.padding_right=15
@@ -29,9 +23,9 @@ do
     script="$PLUGIN_DIR/space.sh"
   )
 
-  sketchybar --add space space.$sid left    \
-             --set space.$sid "${space[@]}" \
-             --subscribe space.$sid mouse.clicked
+  sketchybar --add item space.$sid left             \
+             --set space.$sid "${space[@]}"         \
+             --subscribe space.$sid mouse.clicked aerospace_workspace_change
 done
 
 spaces=(
@@ -41,19 +35,5 @@ spaces=(
   background.drawing=on
 )
 
-separator=(
-  icon=􀆊
-  icon.font="$FONT:Heavy:16.0"
-  padding_left=15
-  padding_right=15
-  label.drawing=off
-  associated_display=active
-  click_script='yabai -m space --create && sketchybar --trigger space_change'
-  icon.color=$WHITE
-)
-
 sketchybar --add bracket spaces '/space\..*/' \
-           --set spaces "${spaces[@]}"        \
-                                              \
-           --add item separator left          \
-           --set separator "${separator[@]}"
+           --set spaces "${spaces[@]}"
